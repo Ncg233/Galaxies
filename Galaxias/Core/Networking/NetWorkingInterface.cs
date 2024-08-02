8a24569a6911a92f17427c5cb6353f353a777644
Ncg233 <2903824519@qq.com>
2024-08-02 16:21:42 +0800

    networking

--- Galaxias/Core/Networking/NetWorkingInterface.cs
+++ Galaxias/Core/Networking/NetWorkingInterface.cs
@@ -1,10 +1,36 @@
-﻿using System;
+﻿using LiteNetLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Galaxias.Core.Networking;
-internal class NetWorkingInterface
+public class NetWorkingInterface
 {
+    protected readonly EventBasedNetListener Listener = new();
+    protected readonly NetManager Manager;
+    public NetPeer? Server { get; private set; }
+
+    protected NetWorkingInterface()
+    {
+
+        Manager = new NetManager(Listener);
+        
+    }
+    public void Connect(string address, int port, string key = "key")
+    {
+        if (Server != null)
+        {
+            return;
+        }
+        Manager.Start();
+        Manager.Connect(address, port, key);
+    }
+    public void Stop()
+    {
+        Manager.Stop();
+    }
+
 }
---
+++ Galaxias/Server/Server.cs
@@ -0,0 +1,22 @@
+﻿using Galaxias.Core.Networking;
+using LiteNetLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxias.Server;
+internal class Server : NetWorkingInterface
+{
+    public Server()
+    {
+        Listener.ConnectionRequestEvent += NewConnection;
+    }
+    private void NewConnection(ConnectionRequest request)
+    {
+        Console.WriteLine("New Connection");
+        request.AcceptIfKey("key");
+    }
+}
+
